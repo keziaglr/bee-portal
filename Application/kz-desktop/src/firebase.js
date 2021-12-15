62d561307d17f9128e954086ae586060a8af9cab
keziaglr <keziagloria24@gmail.com>
2021-12-15 08:04:39 +0700

    first apps

---
+++ Application/kz-desktop/src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js'
+
+export const firebaseApp = initializeApp({
+    apiKey: "AIzaSyD_h_N1tY06VnCUWnCFOtKHEbJGSkTRMDk",
+    authDomain: "kz-desktop.firebaseapp.com",
+    projectId: "kz-desktop",
+    storageBucket: "kz-desktop.appspot.com",
+    messagingSenderId: "231812641457",
+    appId: "1:231812641457:web:f3ea7b5c9338d2ac4a2890",
+    measurementId: "G-8ZKHKC4NKN"
+});
